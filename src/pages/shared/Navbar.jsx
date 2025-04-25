cc9838081996ff167f8db3559ed69cb3bce50109
satyajit <satyajit_jbl@yahoo.com>
2025-04-25 23:11:50 +0600

    navbar allignment with other feathers corrected

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
   return (
     // <nav className=" shadow-md sticky top-0 z-50 bg-slate-200 backdrop-blur-lg ">
     <nav className="h-16 shadow-md sticky top-0 z-50 bg-[#1E3A8A] backdrop-blur-lg ">
-      <div className="container mx-auto px-2 sm:px-6 lg:px-8 ">
+      <div className="w-11/12 mx-auto px-2 sm:px-6 lg:px-8 ">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className=" flex flex-shrink-0 items-center">
